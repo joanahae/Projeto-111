79bd69b441fc4e73b7ad5a7eec970f34799d65cb
joanahae <111794333+joanahae@users.noreply.github.com>
2023-11-07 16:41:46 -0300

    Add files via upload

---
+++ PRO_1-4_C46_AtividadeDoAluno1-main/PRO_1-4_C46_AtividadeDoAluno1-main/move_file.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+from_dir = "c:/Users/mcata/Downloads/Copia Aula 111.py"
+to_dir = "c:/Users/mcata/Desktop/Arquivos_Documentos"
+
+list_of_files = os.listdir(from_dir)
+#print(list_of_files)
+os.path.splitext(from_dir)
